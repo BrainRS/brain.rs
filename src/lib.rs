978f776cfa061609ee0b455e6d1d3a0d02610c75
Adrian Punga <adrian.punga@sqreept.com>
2018-12-23 22:34:08 +0200

    Basic module structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
+pub mod cross_validate;
+pub mod likely;
+pub mod lookup;
+pub mod neural_network;
+pub mod neural_network_gpu;
+pub mod train_stream;
+pub mod recurrent {
+    mod rnn;
+    mod lstm;
+    mod gru;
+    mod rnn_time_step;
+    mod lstm_time_step;
+    mod gru_time_step;
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
