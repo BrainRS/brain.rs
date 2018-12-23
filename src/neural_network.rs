eacb5028f3c8e4cb46151e64e3f6728135ac14b1
Adrian Punga <adrian.punga@sqreept.com>
2018-12-23 23:42:49 +0200

    Add network options

--- src/neural_network.rs
+++ src/neural_network.rs
@@ -1,19 +1,25 @@
 use std::time::Duration;
 
-#[derive(Debug)]
 pub struct NeuralNetwork {
+    _options: NeuralNetworkOptions,
 }
 
 pub struct NeuralNetworkOptions {
-    pub iterations: u32,              // the maximum times to iterate the training data
-    pub error_thresh: f64,             // the acceptable error percentage from training data
-    pub log: bool,                    // true to use console.log, when a function is supplied it is used
-    pub log_period: u32,               // iterations between logging out
-    pub learning_rate: f64,            // multiply's against the input and the delta then adds to momentum
-    pub momentum: f64,                // multiply's against the specified "change" then adds to learning rate for change
-    pub callback: Option<Box<Fn()>>,  // a periodic call back that can be triggered while training
-    pub callback_period: u32,          // the number of iterations through the training data between callback calls
-    pub timeout: Option<Duration>,    // the max number of milliseconds to train for
+    // network options
+    pub leaky_relu_alpha: f64,
+    pub binary_thresh: f64,
+    pub hidden_layers: Option<Vec<u32>>,  // array of ints for the sizes of the hidden layers in the network
+    pub activation: String,              // supported activation types ['sigmoid', 'relu', 'leaky-relu', 'tanh']
+    // training options
+    pub iterations: u32,                 // the maximum times to iterate the training data
+    pub error_thresh: f64,               // the acceptable error percentage from training data
+    pub log: bool,                       // true to use console.log, when a function is supplied it is used
+    pub log_period: u32,                 // iterations between logging out
+    pub learning_rate: f64,              // multiply's against the input and the delta then adds to momentum
+    pub momentum: f64,                   // multiply's against the specified "change" then adds to learning rate for change
+    pub callback: Option<Box<Fn()>>,     // a periodic call back that can be triggered while training
+    pub callback_period: u32,            // the number of iterations through the training data between callback calls
+    pub timeout: Option<Duration>,       // the max number of milliseconds to train for
     pub praxis: Option<String>,
     pub beta1: f64,
     pub beta2: f64,
@@ -23,15 +29,19 @@ pub struct NeuralNetworkOptions {
 impl Default for NeuralNetworkOptions {
     fn default() -> NeuralNetworkOptions {
         NeuralNetworkOptions {
-            iterations: 20000,    // the maximum times to iterate the training data
-            error_thresh: 0.005,   // the acceptable error percentage from training data
-            log: false,           // true to use console.log, when a function is supplied it is used
-            log_period: 10,        // iterations between logging out
-            learning_rate: 0.3,    // multiply's against the input and the delta then adds to momentum
-            momentum: 0.1,        // multiply's against the specified "change" then adds to learning rate for change
-            callback: None,       // a periodic call back that can be triggered while training
-            callback_period: 10,   // the number of iterations through the training data between callback calls
-            timeout: None,        // the max number of milliseconds to train for
+            leaky_relu_alpha: 0.01,
+            binary_thresh: 0.5,
+            hidden_layers: None,
+            activation: String::from("sigmoid"),
+            iterations: 20000,
+            error_thresh: 0.005,
+            log: false,
+            log_period: 10,
+            learning_rate: 0.3,
+            momentum: 0.1,
+            callback: None,
+            callback_period: 10,
+            timeout: None,
             praxis: None,
             beta1: 0.9,
             beta2: 0.999,
@@ -40,7 +50,9 @@ impl Default for NeuralNetworkOptions {
     }
 }
 impl NeuralNetwork {
-    pub fn new(_options: NeuralNetworkOptions) -> NeuralNetwork {
-        return NeuralNetwork {}
+    pub fn new(options: NeuralNetworkOptions) -> NeuralNetwork {
+        return NeuralNetwork {
+            _options: options,
+        }
     }
 }
